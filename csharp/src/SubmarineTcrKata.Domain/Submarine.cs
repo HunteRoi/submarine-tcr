fa397ddad51757f36e3e7b63fa298fe03cb3057f
Mx <fx@pluriell.app>
2022-11-24 19:02:33 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 11
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- csharp/src/SubmarineTcrKata.Domain/Submarine.cs
+++ csharp/src/SubmarineTcrKata.Domain/Submarine.cs
@@ -4,15 +4,8 @@ public class Submarine : ISubmarine
 {
     public void ExecuteCommand(string command)
     {
-        Aim += command switch
-        {
-            "down 1" => 1,
-            "down 3" => 3,
-            "down 2" => 2,
-            "down 5" => 5,
-            "down 8" => 8,
-            _ => 7
-        };
+        
+        Aim += int.Parse(command.Split(" ")[1]);
     }
 
     public int Aim
